8d1e471824d00942bcb77c2b0db654fbf5e17592
Yehuda Sadeh <yehuda@redhat.com>
2015-03-26 21:25:54 -0700

    rgw: apply redirect rules in static website
    only when key matches, not checking for return code yet.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1062,6 +1062,8 @@ struct req_state {
    string region_endpoint;
    string bucket_instance_id;
 
+   string redirect;
+
    RGWBucketInfo bucket_info;
    map<string, bufferlist> bucket_attrs;
    bool bucket_exists;
