b9d3b604594feadf1a6e42b2baabbfe10e06ffd5
Yehuda Sadeh <yehuda@redhat.com>
2016-09-27 14:35:31 -0700

    rgw_admin: better bucket reshard logging
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>
    (cherry picked from commit ac88b5d9e6bc3c8b59c500bf79a0e3daa923a47c)
    See: http://tracker.ceph.com/issues/17556
    See: https://github.com/ceph/ceph/pull/11368
    Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>
    (cherry picked from commit 4adb247e56e8e77cbdfecca8f90240edbd04742f)
    Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -5064,6 +5064,8 @@ next:
 
     formatter->open_array_section("entries");
 
+    uint64_t total_entries = 0;
+
     for (int i = 0; i < num_source_shards; ++i) {
       bool is_truncated = true;
       marker.clear();
@@ -5077,6 +5079,12 @@ next:
 
         list<rgw_cls_bi_entry>::iterator iter;
         for (iter = entries.begin(); iter != entries.end(); ++iter) {
+          formatter->open_object_section("entry");
+
+          encode_json("shard_id", i, formatter);
+          encode_json("num_entry", total_entries, formatter);
+          total_entries++;
+
           rgw_cls_bi_entry& entry = *iter;
           encode_json("entry", entry, formatter);
           marker = entry.idx;
@@ -5100,9 +5108,9 @@ next:
           if (ret < 0) {
             return ret;
           }
+          formatter->close_section();
+          formatter->flush(cout);
         }
-
-        formatter->flush(cout);
       }
     }
     formatter->close_section();
