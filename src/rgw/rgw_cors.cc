d3d6bd1c217a490c5c6605ccce3e9758a2b393f6
Yao Zongyou <yaozongyou@vip.qq.com>
2017-12-13 15:42:58 +0800

    rgw: implement RGWCORSConfiguration::dump and RGWCORSRule::dump function
    Signed-off-by: Yao Zongyou <yaozongyou@vip.qq.com>
    (cherry picked from commit a144316001008b6dcb7c42f4f656ba46110e976a)
    Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

--- src/rgw/rgw_cors.cc
+++ src/rgw/rgw_cors.cc
@@ -21,6 +21,7 @@
 #include "include/types.h"
 #include "common/debug.h"
 #include "include/str_list.h"
+#include "common/ceph_json.h"
 #include "common/Formatter.h"
 
 #include "rgw_cors.h"
@@ -50,6 +51,15 @@ void RGWCORSRule::erase_origin_if_present(string& origin, bool *rule_empty) {
     allowed_origins.erase(it);
     *rule_empty = (allowed_origins.empty());
   }
+}
+
+void RGWCORSRule::dump(Formatter *f) const {
+  encode_json("max_age", max_age, f);
+  encode_json("allowed_methods", static_cast<unsigned>(allowed_methods), f);
+  encode_json("id", id, f);
+  encode_json("allowed_hdrs", allowed_hdrs, f);
+  encode_json("allowed_origins", allowed_origins, f);
+  encode_json("exposable_hdrs", exposable_hdrs, f);
 }
 
 /*
@@ -177,6 +187,10 @@ void RGWCORSConfiguration::erase_host_name_rule(string& origin) {
       break;
     }
   }
+}
+
+void RGWCORSConfiguration::dump(Formatter *f) const {
+  encode_json("rules", rules, f);
 }
 
 void RGWCORSConfiguration::dump() {
