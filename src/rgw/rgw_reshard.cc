d848987e9f0dda06d354b8715ea89af0d4c4378b
Orit Wasserman <owasserm@redhat.com>
2018-01-21 10:12:43 +0200

    rgw: bucket resharding should rewrite bucket attrs after link
    bucket link op resets bucket ACL attr.
    
    Fixes: http://tracker.ceph.com/issues/22742
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -346,7 +346,7 @@ public:
 
 int RGWBucketReshard::do_reshard(
 		   int num_shards,
-		   const RGWBucketInfo& new_bucket_info,
+		   RGWBucketInfo& new_bucket_info,
 		   int max_entries,
                    bool verbose,
                    ostream *out,
@@ -481,6 +481,12 @@ int RGWBucketReshard::do_reshard(
   if (ret < 0) {
     lderr(store->ctx()) << "failed to link new bucket instance (bucket_id=" << new_bucket_info.bucket.bucket_id << ": " << err << "; " << cpp_strerror(-ret) << ")" << dendl;
     return -ret;
+  }
+
+  ret = rgw_bucket_set_attrs(store, new_bucket_info, bucket_attrs, &new_bucket_info.objv_tracker);
+  if (ret < 0) {
+    lderr(store->ctx()) << "failed to reset attrs for bucket " << new_bucket_info.bucket.name << " instance (bucket_id=" << new_bucket_info.bucket.bucket_id << ": " << err << "; " << cpp_strerror(-ret) << ")" << dendl;
+    /* don't error out, reshard process succeeded */
   }
 
   ret = bucket_info_updater.complete();
